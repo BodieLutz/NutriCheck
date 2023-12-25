e149f46a38bbfc66e930aff1407ecba78b88ab59
BodieLutz <bodielutz@mines.edu>
2023-12-25 14:26:44 -0700

    Added database credentials and connection

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+
+function getConnection(){
+    include 'database_credentials.php';
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+  
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+
+?>
---
+++ database_credentials.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "localhost";
+$username = "bodielutz"; // put your user name here. Should match your mines multipass.
+$password = "b2l!GlIaqz4AhXnZ"; // put your password here
+$database = "nutricheck"; // put your database name here 
+?>
