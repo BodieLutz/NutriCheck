7290df47546c3d57c8f3fa2cec0bc3547dd7a0fe
BodieLutz <bodielutz@mines.edu>
2024-02-19 18:47:05 -0700

    Styled the search page

--- search.php
+++ search.php
@@ -13,13 +13,15 @@
         <div class="row">
             <div class="col" id="form-div">
                 <form id="ing-form">
-                    <fieldset>
+                    <fieldset class="ingredient">
                         <legend>Ingredient</legend>
-                        <input type="text" name="ing-name" id="ing-name">
-                        <label for="ing-name">Name: </label><br>
+                        <label for="ing-name">Name: </label>
+                        <input type="text" name="ing-name" id="ing-name" class="box_input ingredient">
+                        <br><br><br>
 
-                        <input type="number" name="ing-cals" id="ing-cals">
-                        <label for="ing-cals">Calories: </label><br>
+                        <label for="ing-cals">Calories: </label>
+                        <input type="number" name="ing-cals" id="ing-cals" class="box_input ingredient">
+                        <br>
 
                     </fieldset>
                 </form>
@@ -27,17 +29,21 @@
                 <form id="recipe-form">
                     <fieldset>
                         <legend>Recipe</legend>
-                        <input type="text" name="recipe-name" id="recipe-name">
-                        <label for="recipe-name">Name: </label><br>
+                        <label for="recipe-name">Name: </label>
+                        <input type="text" name="recipe-name" id="recipe-name" class="box_input recipe">
+                        <br><br><br>
 
-                        <input type="text" name="recipe-goal" id="recipe-goal">
-                        <label for="recipe-goal">Goal: </label><br>
+                        <label for="recipe-goal">Goal: </label>
+                        <input type="text" name="recipe-goal" id="recipe-goal" class="box_input recipe">
+                        <br><br><br>
 
-                        <input type="number" name="recipe-cals" id="recipe-cals">
-                        <label for="recipe-cals">Calories: </label><br>
+                        <label for="recipe-cals">Calories: </label>
+                        <input type="number" name="recipe-cals" id="recipe-cals" class="box_input recipe">
+                        <br><br><br>
 
-                        <input type="number" name="recipe-protein" id="recipe-protein">
-                        <label for="recipe-protein">Protein: </label><br>
+                        <label for="recipe-protein">Protein: </label>
+                        <input type="number" name="recipe-protein" id="recipe-protein" class="box_input recipe">
+                        <br>
 
                     </fieldset>
                 </form>
